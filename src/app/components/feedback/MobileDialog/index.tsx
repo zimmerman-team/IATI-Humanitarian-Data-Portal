b2c76b38b3cac540b3e437fa04404a2a7050c186
Juan Albergen <juan@zimmermanzimmerman.nl>
2019-07-24 17:12:58 +0200

    refactor: usage of theme colours

--- src/app/components/feedback/MobileDialog/index.tsx
+++ src/app/components/feedback/MobileDialog/index.tsx
@@ -1,23 +1,25 @@
 import React from 'react';
 import styled from 'styled-components';
+import colors from 'app/theme/color';
 import Typography from '@material-ui/core/Typography';
-import {Visibility} from '@material-ui/icons';
+import { Visibility }  from '@material-ui/icons';
+
 
 const Message = "Mobiles are great! But, not so for viewing large data sets. Revist the page on a larger device to see the data, all the content, and graphs.";
 
 const ContentContainer = styled.div`
  display: flex;
  justify-content: center;
- background-color: #6e5acc;
+ background-color: ${colors.purplebase};
  padding: 24px;
 `;
 
 const Text = styled(props => <Typography {...props} />)`
-  color: white;
+ color: ${colors.whiteOrFontlightbase};
 `;
 
 const Icon = styled(props => <Visibility {...props} />)`
- color: white;
+ color: ${colors.whiteOrFontlightbase};
  margin-top: 16px;
  margin-right: 24px;
  
