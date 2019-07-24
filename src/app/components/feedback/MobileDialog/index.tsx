84ab25b6cd2603f9ab152f074a32f7e365470911
Juan Albergen <juan@zimmermanzimmerman.nl>
2019-07-24 16:59:48 +0200

    feat: implementation of Mobile Dialog

---
+++ src/app/components/feedback/MobileDialog/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import styled from 'styled-components';
+import Typography from '@material-ui/core/Typography';
+import {Visibility} from '@material-ui/icons';
+
+const Message = "Mobiles are great! But, not so for viewing large data sets. Revist the page on a larger device to see the data, all the content, and graphs.";
+
+const ContentContainer = styled.div`
+ display: flex;
+ justify-content: center;
+ background-color: #6e5acc;
+ padding: 24px;
+`;
+
+const Text = styled(props => <Typography {...props} />)`
+  color: white;
+`;
+
+const Icon = styled(props => <Visibility {...props} />)`
+ color: white;
+ margin-top: 16px;
+ margin-right: 24px;
+ 
+ && {
+ height: 1.7em;
+ width: 1.7em;
+ }
+`;
+
+const MobileDialog = () => {
+  return (
+      <ContentContainer>
+        <Icon/>
+        <Text variant="body2">{Message}</Text>
+      </ContentContainer>
+  );
+};
+export default MobileDialog;
