c031f4eecd41217e01146e840e951f8a599cdb56
Jim Fung <jim@zimmermanzimmerman.nl>
2019-07-25 14:20:45 +0200

    refactor: solved issue with nivo library and ts definitions

--- src/app/components/charts/BarCharts/HorizontalBarChart/index.tsx
+++ src/app/components/charts/BarCharts/HorizontalBarChart/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ResponsiveBar } from '@nivo/bar'
+import { ResponsiveBar, BarSvgProps } from '@nivo/bar';
 import { HorizontalBarChartModel } from './model';
 import styled from 'styled-components';
 import colors from 'app/theme/color';
@@ -12,68 +12,74 @@ import { Visibility } from '@material-ui/icons';
 // website examples showcase many properties,
 // you'll often use just a few of them.
 
-export const MyResponsiveBar = ({ data /* see data tab */ }) => (
-  <ResponsiveBar
-    data={data}
-    keys={[ 'hot dog', 'burger', 'sandwich', 'kebab', 'fries', 'donut' ]}
-    indexBy="country"
-    margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
-    padding={0.3}
-    layout="horizontal"
-    colors={{ scheme: 'nivo' }}
-    defs={[
-      {
-        id: 'dots',
-        type: 'patternDots',
-        background: 'inherit',
-        color: '#38bcb2',
-        size: 4,
-        padding: 1,
-        stagger: true
+const barModel: BarSvgProps = {
+  data: [],
+  keys: ['hot dog', 'burger', 'sandwich', 'kebab', 'fries', 'donut'],
+  indexBy: 'country',
+  margin: { top: 50, right: 130, bottom: 50, left: 60 },
+  padding: 0.3,
+  layout: 'horizontal',
+  colors: { scheme: 'nivo' },
+  defs: [
+    {
+      id: 'dots',
+      type: 'patternDots',
+      background: 'inherit',
+      color: '#38bcb2',
+      size: 4,
+      padding: 1,
+      stagger: true,
+    },
+    {
+      id: 'lines',
+      type: 'patternLines',
+      background: 'inherit',
+      color: '#eed312',
+      rotation: -45,
+      lineWidth: 6,
+      spacing: 10,
+    },
+  ],
+  fill: [
+    {
+      match: {
+        id: 'fries',
       },
-      {
-        id: 'lines',
-        type: 'patternLines',
-        background: 'inherit',
-        color: '#eed312',
-        rotation: -45,
-        lineWidth: 6,
-        spacing: 10
-      }
-    ]}
-    fill={[
-      {
-        match: {
-          id: 'fries'
-        },
-        id: 'dots'
+      id: 'dots',
+    },
+    {
+      match: {
+        id: 'sandwich',
       },
-      {
-        match: {
-          id: 'sandwich'
-        },
-        id: 'lines'
-      }
-    ]}
-    borderColor={{ from: 'color', modifiers: [ [ 'darker', 1.6 ] ] }}
-    axisTop={{ tickSize: 5, tickPadding: 5, tickRotation: 0, legend: '', legendOffset: 36 }}
-    axisRight={null}
-    axisBottom={null}
-    axisLeft={{
-      tickSize: 5,
-      tickPadding: 5,
-      tickRotation: 0,
-      legend: 'food',
-      legendPosition: 'middle',
-      legendOffset: -40
-    }}
-    enableGridX={true}
-    labelSkipWidth={9}
-    labelSkipHeight={17}
-    labelTextColor={{ from: 'color', modifiers: [ [ 'darker', 1.6 ] ] }}
-    legends={[]}
-    animate={true}
-    motionStiffness={90}
-    motionDamping={15}
-  />
-);
+      id: 'lines',
+    },
+  ],
+
+  axisTop: {
+    tickSize: 5,
+    tickPadding: 5,
+    tickRotation: 0,
+    legend: '',
+    legendOffset: 36,
+  },
+  axisRight: null,
+  axisBottom: null,
+  axisLeft: {
+    tickSize: 5,
+    tickPadding: 5,
+    tickRotation: 0,
+    legend: 'food',
+    legendPosition: 'middle',
+    legendOffset: -40,
+  },
+  labelSkipWidth: 9,
+  labelSkipHeight: 17,
+  labelTextColor: { from: 'color', modifiers: [['darker', 1.6]] },
+  legends: [],
+  motionStiffness: 90,
+  motionDamping: 15,
+};
+
+export const MyResponsiveBar = ({ data }) => {
+  return <ResponsiveBar {...barModel} data={data} />;
+};
