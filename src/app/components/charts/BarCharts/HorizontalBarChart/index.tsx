7b1a14ab73b4db6782cd5383baba75a9ca94f3f7
Juan Albergen <juan@zimmermanzimmerman.nl>
2019-07-25 16:57:19 +0200

    feat: incr changes on horizontal bar chart

--- src/app/components/charts/BarCharts/HorizontalBarChart/index.tsx
+++ src/app/components/charts/BarCharts/HorizontalBarChart/index.tsx
@@ -2,9 +2,7 @@ import React from 'react';
 import { ResponsiveBar, BarSvgProps } from '@nivo/bar';
 import { HorizontalBarChartModel } from './model';
 import styled from 'styled-components';
-import colors from 'app/theme/color';
-import Typography from '@material-ui/core/Typography';
-import { Visibility } from '@material-ui/icons';
+
 
 // make sure parent container have a defined height when using
 // responsive component, otherwise height will be 0 and
@@ -14,47 +12,11 @@ import { Visibility } from '@material-ui/icons';
 
 const barModel: BarSvgProps = {
   data: [],
-  keys: ['hot dog', 'burger', 'sandwich', 'kebab', 'fries', 'donut'],
-  indexBy: 'country',
-  margin: { top: 50, right: 130, bottom: 50, left: 60 },
-  padding: 0.3,
+  keys: ['percentage'],
+  indexBy: 'name',
+  margin: { top: 50, right: 130, bottom: 50, left: 100 },
+  padding: 0.4,
   layout: 'horizontal',
-  colors: { scheme: 'nivo' },
-  defs: [
-    {
-      id: 'dots',
-      type: 'patternDots',
-      background: 'inherit',
-      color: '#38bcb2',
-      size: 4,
-      padding: 1,
-      stagger: true,
-    },
-    {
-      id: 'lines',
-      type: 'patternLines',
-      background: 'inherit',
-      color: '#eed312',
-      rotation: -45,
-      lineWidth: 6,
-      spacing: 10,
-    },
-  ],
-  fill: [
-    {
-      match: {
-        id: 'fries',
-      },
-      id: 'dots',
-    },
-    {
-      match: {
-        id: 'sandwich',
-      },
-      id: 'lines',
-    },
-  ],
-
   axisTop: {
     tickSize: 5,
     tickPadding: 5,
@@ -68,18 +30,40 @@ const barModel: BarSvgProps = {
     tickSize: 5,
     tickPadding: 5,
     tickRotation: 0,
-    legend: 'food',
+    legend: '',
     legendPosition: 'middle',
     legendOffset: -40,
   },
   labelSkipWidth: 9,
   labelSkipHeight: 17,
-  labelTextColor: { from: 'color', modifiers: [['darker', 1.6]] },
   legends: [],
   motionStiffness: 90,
   motionDamping: 15,
+
+  enableGridX: true,
+  enableGridY: true,
+  maxValue: 100,
 };
 
+const BarChart = styled(props => <ResponsiveBar {...props} />)`
+svg > g > g{
+color: black !important;
+}
+`;
+
+const CustomRect = styled(props => <rect {...props} />)`
+  height: 24px;
+  fill: #5accbf;
+`;
+
+// const CustomBarComponent = () => {
+//   return (
+//     <CustomRect />
+//   )
+// };
+
+// https://nivo.rocks/bar/
 export const MyResponsiveBar = ({ data }) => {
-  return <ResponsiveBar {...barModel} data={data} />;
+  return <BarChart {...barModel} data={data} barComponent={CustomRect} />;
+  // return <BarChart {...barModel} data={data} />;
 };
--- src/app/components/charts/BarCharts/HorizontalBarChart/story.tsx
+++ src/app/components/charts/BarCharts/HorizontalBarChart/story.tsx
@@ -2,13 +2,12 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { MyResponsiveBar } from './index';
 import Providers from 'app/Providers';
-import { Grid } from '@material-ui/core';
 import { mockData, nivoMock } from './mock'
 
 storiesOf('Charts|Bar Charts/', module).add('Horizontal Bar Chart', () => (
   <Providers>
-    <div style={{height: '1000px', width: '1000px'}}>
-      <MyResponsiveBar data={nivoMock}/>
+    <div style={{height: '380px', width: '1000px'}}>
+      <MyResponsiveBar data={mockData}/>
     </div>
   </Providers>
 ));
