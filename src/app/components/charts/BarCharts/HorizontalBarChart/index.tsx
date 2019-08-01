c3722a9c6ebe38bd0d3a0734a47751e755586c08
Jim Fung <jim@zimmermanzimmerman.nl>
2019-08-01 11:44:30 +0200

    refactor: modifications related to PR review

--- src/app/components/charts/BarCharts/HorizontalBarChart/index.tsx
+++ src/app/components/charts/BarCharts/HorizontalBarChart/index.tsx
@@ -3,7 +3,7 @@ import { ResponsiveBar, BarSvgProps } from '@nivo/bar';
 import styled from 'styled-components';
 import Colours from 'app/theme/color';
 import { colorScheme } from 'app/components/charts/BarCharts/common/colorUtil';
-import { HorizontalBarChartModel } from './model';
+import { HorizontalBarChartModel, barModel } from './model';
 
 //TODO:
 //  - Find a way to implement the colouring.
@@ -15,85 +15,48 @@ import { HorizontalBarChartModel } from './model';
 // website examples showcase many properties,
 // you'll often use just a few of them.
 
-const barModel: BarSvgProps = {
-  data: [],
-  keys: ['percentage'],
-  indexBy: 'name',
-  margin: { top: 32, right: 50, bottom: 0, left: 120 },
-  padding: 0.4,
-  layout: 'horizontal',
-  axisTop: {
-    tickValues: 5,
-    tickSize: 5,
-    tickPadding: 5,
-    tickRotation: 0,
-    legend: '',
-    legendOffset: 36,
-    format: v => `${v}%`
-  },
-  axisRight: null,
-  axisBottom: null,
-  axisLeft: {
-    tickSize: 5,
-    tickPadding: 5,
-    tickRotation: 0,
-    legend: '',
-    legendPosition: 'middle',
-    legendOffset: -40,
-  },
-  labelSkipWidth: 9,
-  labelSkipHeight: 17,
-  legends: [],
-  motionStiffness: 90,
-  motionDamping: 15,
-  colors: [],
-
-  enableGridX: true,
-  enableGridY: true,
-  maxValue: 100,
-
-  theme: {
-    axis: {
-      ticks: {
-        text: {
-          fontWeight: 500,
-          fontFamily: "Inter",
-          fontSize: 12,
-        }
-      }
-    },
-    legends: {
-      text: {
-        fontWeight: 500,
-        fontFamily: "Inter",
-        fontSize: 12,
-      }
-    }
-  }
-};
-
 const BarComponent = props => {
-  return(
+  return (
     <g {...props}>
       <rect {...props} fill={Colours.primaryBase} height={props.height / 2} />
-      <text {...props} x={props.width - 64} y={props.y - 5} fontFamily="Inter" fontSize="12px" lineHeight="1.33" letterSpacing="0.42" fontWeight="normal" fill={Colours.greydark20OrFontsecondary}>{props.data.data.percentage}% ({props.data.data.value})</text>
+      <text
+        {...props}
+        x={props.width - 64}
+        y={props.y - 5}
+        fontFamily="Inter"
+        fontSize="12px"
+        lineHeight="1.33"
+        letterSpacing="0.42"
+        fontWeight="normal"
+        fill={Colours.greydark20OrFontsecondary}
+      >
+        {props.data.data.percentage}% ({props.data.data.value})
+      </text>
     </g>
   );
 };
 
-const BarChart = styled(props => <ResponsiveBar {...props} />)`&&{}`;
+const BarChart = styled(props => <ResponsiveBar {...props} />)`
+  && {
+  }
+`;
 
 //TODO: Chart container should adapt to the width of the card that it is in for responsiveness.
 const ChartContainer = styled.div`
   height: 270px;
-  width: 1000px
+  width: 1000px;
 `;
 
 // https://nivo.rocks/bar/
 export const HorizontalBarChart = (props: HorizontalBarChartModel) => {
-  return(
+  return (
     <ChartContainer>
-      <BarChart {...barModel} data={props.values} colors={colorScheme(props.colors)} barComponent={BarComponent} />
+      <BarChart
+        {...barModel}
+        data={props.values}
+        colors={colorScheme(props.colors)}
+        barComponent={BarComponent}
+      />
     </ChartContainer>
   );
 };
