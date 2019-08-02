c5ac3ab7b51aef398f1022f2ae3e999a70c09835
Jim Fung <jim@zimmermanzimmerman.nl>
2019-08-02 18:26:56 +0200

    feat: layout of signatory data

---
+++ src/app/modules/signatory-data/model.ts
@@ -0,0 +1,20 @@
+type HumanitarianCheckup = {
+  primary: boolean | string;
+  secondary: boolean | string;
+  tertiary: boolean | string;
+  quaternary: boolean | string;
+};
+
+type Signatory = {
+  publishinOrganisation: string;
+  gbSignatory: string;
+  organisationType: string;
+  iatiVersion: number;
+  humanitarianData: HumanitarianCheckup;
+};
+export type SignatoryDataModel = {
+  title: string;
+  description: string;
+  signatories: Signatory[];
+};
+
