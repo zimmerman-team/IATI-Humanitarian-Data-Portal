b6db77909c58619c3ff39e8a66396fca99d0a52c
Jim Fung <jim@zimmermanzimmerman.nl>
2019-08-07 13:36:33 +0200

    feat: added title to landing module

--- src/app/modules/landing/index.tsx
+++ src/app/modules/landing/index.tsx
@@ -2,7 +2,12 @@ import React from 'react';
 import { Container, Typography } from '@material-ui/core';
 import { Grid, Row } from 'react-styled-flexboxgrid';
 import { LandingLayout } from 'app/modules/landing/layout';
+import { signatoryDataMock } from '../signatory-data/mock';
+import useTitle from 'react-use/lib/useTitle';
+import { DebugBox } from 'app/utils/layout';
 
 function Landing() {
+  useTitle(`MLT - Home`);
+
   return <LandingLayout />;
 }
