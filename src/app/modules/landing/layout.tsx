8002858ce6c338077fbc03354ffeb4ea9629e54c
Jim Fung <jim@zimmermanzimmerman.nl>
2019-08-07 13:36:47 +0200

    feat: added styling

--- src/app/modules/landing/layout.tsx
+++ src/app/modules/landing/layout.tsx
@@ -1,18 +1,29 @@
+/* eslint-disable react/jsx-max-depth */
 // core
 import React from 'react';
 import styled from 'styled-components';
+import Highlighter from 'react-highlight-words';
 // utils
 import { Container, Grid, Typography, Box } from '@material-ui/core';
 // mock data
 import { mockData } from './common/StatContainer/mock';
 // comps
 import { StatContainer } from 'app/modules/landing/common/StatContainer/StatContainer';
+import { DebugBox } from 'app/utils/layout';
 
 const LandingPaper = styled.div`
   background-color: white;
-  /* outline: 1px solid black; */
   max-width: 987px;
   padding: 40px;
+`;
+
+const LandingIntrotext = styled(Typography)`
+  mark {
+    color: white;
+    background: #5accbf;
+    padding-left: 5px;
+    padding-right: 5px;
+  }
 `;
 
 export const LandingLayout = () => {
@@ -21,9 +32,16 @@ export const LandingLayout = () => {
       <LandingPaper>
         <Grid container direction="column">
           <Grid item md={10}>
-            <Typography variant="h1">
-              A Spotlight on International Humanitarian Assistance Information
-            </Typography>
+            <Box width="99%">
+              <LandingIntrotext variant="h1">
+                <Highlighter
+                  highlightClassName="YourHighlightClass"
+                  searchWords={['Humanitarian']}
+                  autoEscape
+                  textToHighlight="A Spotlight on International Humanitarian Assistance Information"
+                />
+              </LandingIntrotext>
+            </Box>
           </Grid>
           <Box height="40px" />
           <Grid item md={7}>
