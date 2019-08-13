ae3321ea1e07a672743163f78283522ab705dac2
Juan Albergen <juan@zimmermanzimmerman.nl>
2019-08-13 13:14:26 +0200

    feat: implementation of signatory data/activity list, inc responsiveness

---
+++ src/app/modules/signatory-data/submodules/activityList/model.ts
@@ -0,0 +1,7 @@
+import { TableModuleModel } from 'app/components/datadisplay/Table/model';
+
+export type ActivityListLayoutModel = {
+  title: string;
+  subtitle: string;
+  activity: TableModuleModel;
+};
