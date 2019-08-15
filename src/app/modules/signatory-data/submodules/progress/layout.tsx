6b1847055a623ee7ae7af4ca88323968d05c3e96
Jim Fung <jim@zimmermanzimmerman.nl>
2019-08-15 16:40:07 +0200

    feat: added signatory data / progress layout

---
+++ src/app/modules/signatory-data/submodules/progress/layout.tsx
@@ -0,0 +1,12 @@
+import { Container, Grid } from '@material-ui/core';
+import React from 'react';
+
+export const ProgressLayout = () => {
+  return (
+    <Container>
+      <Grid container>
+        <Grid item></Grid>
+      </Grid>
+    </Container>
+  );
+};
