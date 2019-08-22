00e2e92923467d06f4609049fd7fdfde4ebf821b
Juan Albergen <juan@zimmermanzimmerman.nl>
2019-08-22 11:00:10 +0200

    refactor: correct data implementation of the LineChart and its card

--- src/app/components/charts/LineChart/mock.ts
+++ src/app/components/charts/LineChart/mock.ts
@@ -1,356 +1,354 @@
 import { LineChartModel } from './model';
 
-export const mockData: LineChartModel =
-  {
-    values: [
-      {
-        id: "Signatories publish to IATI",
-        data: [
-          {
-            x: "31 June 2017",
-            y: 12,
-          },
-          {
-            x: "31 Dec 2017",
-            y: 16,
-          },
-          {
-            x: "31 Apr 2018",
-            y: 22,
-          },
-          {
-            x: "31 Dec 2018",
-            y: 30,
-          },
-          {
-            x: "31 May 2019",
-            y: 35,
-          },
-        ],
-      },
-      {
-        id: "Provides granular v2.02 data",
-        data: [
-          {
-            x: "31 June 2017",
-            y: 1,
-          },
-          {
-            x: "31 Dec 2017",
-            y: 20,
-          },
-          {
-            x: "31 Apr 2018",
-            y: 5,
-          },
-          {
-            x: "31 Dec 2018",
-            y: 60,
-          },
-          {
-            x: "31 May 2019",
-            y: 80,
-          },
-        ],
-      },
-      {
-        id: "Publishing hum. activity data",
-        data: [
-          {
-            x: "31 June 2017",
-            y: 20,
-          },
-          {
-            x: "31 Dec 2017",
-            y: 30,
-          },
-          {
-            x: "31 Apr 2018",
-            y: 40,
-          },
-          {
-            x: "31 Dec 2018",
-            y: 50,
-          },
-          {
-            x: "31 May 2019",
-            y: 60,
-          },
-        ],
-      },
-    ]
-  }
-;
+export const mockData: LineChartModel = {
+  values: [
+    {
+      id: 'Signatories publish to IATI',
+      data: [
+        {
+          x: '31 June 2017',
+          y: 12,
+        },
+        {
+          x: '31 Dec 2017',
+          y: 16,
+        },
+        {
+          x: '31 Apr 2018',
+          y: 22,
+        },
+        {
+          x: '31 Dec 2018',
+          y: 30,
+        },
+        {
+          x: '31 May 2019',
+          y: 35,
+        },
+      ],
+    },
+    {
+      id: 'Provides granular v2.02 data',
+      data: [
+        {
+          x: '31 June 2017',
+          y: 1,
+        },
+        {
+          x: '31 Dec 2017',
+          y: 20,
+        },
+        {
+          x: '31 Apr 2018',
+          y: 5,
+        },
+        {
+          x: '31 Dec 2018',
+          y: 60,
+        },
+        {
+          x: '31 May 2019',
+          y: 80,
+        },
+      ],
+    },
+    {
+      id: 'Publishing hum. activity data',
+      data: [
+        {
+          x: '31 June 2017',
+          y: 20,
+        },
+        {
+          x: '31 Dec 2017',
+          y: 30,
+        },
+        {
+          x: '31 Apr 2018',
+          y: 40,
+        },
+        {
+          x: '31 Dec 2018',
+          y: 50,
+        },
+        {
+          x: '31 May 2019',
+          y: 60,
+        },
+      ],
+    },
+  ],
+};
 
 export const nivoMock = [
   {
-    "id": "japan",
-    "color": "hsl(124, 70%, 50%)",
-    "data": [
+    id: 'japan',
+    color: 'hsl(124, 70%, 50%)',
+    data: [
       {
-        "x": "plane",
-        "y": 14
+        x: 'plane',
+        y: 14,
       },
       {
-        "x": "helicopter",
-        "y": 13
+        x: 'helicopter',
+        y: 13,
       },
       {
-        "x": "boat",
-        "y": 106
+        x: 'boat',
+        y: 106,
       },
       {
-        "x": "train",
-        "y": 15
+        x: 'train',
+        y: 15,
       },
       {
-        "x": "subway",
-        "y": 279
+        x: 'subway',
+        y: 279,
       },
       {
-        "x": "bus",
-        "y": 8
+        x: 'bus',
+        y: 8,
       },
       {
-        "x": "car",
-        "y": 80
+        x: 'car',
+        y: 80,
       },
       {
-        "x": "moto",
-        "y": 32
+        x: 'moto',
+        y: 32,
       },
       {
-        "x": "bicycle",
-        "y": 299
+        x: 'bicycle',
+        y: 299,
       },
       {
-        "x": "horse",
-        "y": 41
+        x: 'horse',
+        y: 41,
       },
       {
-        "x": "skateboard",
-        "y": 281
+        x: 'skateboard',
+        y: 281,
       },
       {
-        "x": "others",
-        "y": 163
-      }
-    ]
+        x: 'others',
+        y: 163,
+      },
+    ],
   },
   {
-    "id": "france",
-    "color": "hsl(277, 70%, 50%)",
-    "data": [
+    id: 'france',
+    color: 'hsl(277, 70%, 50%)',
+    data: [
       {
-        "x": "plane",
-        "y": 216
+        x: 'plane',
+        y: 216,
       },
       {
-        "x": "helicopter",
-        "y": 262
+        x: 'helicopter',
+        y: 262,
       },
       {
-        "x": "boat",
-        "y": 217
+        x: 'boat',
+        y: 217,
       },
       {
-        "x": "train",
-        "y": 163
+        x: 'train',
+        y: 163,
       },
       {
-        "x": "subway",
-        "y": 299
+        x: 'subway',
+        y: 299,
       },
       {
-        "x": "bus",
-        "y": 279
+        x: 'bus',
+        y: 279,
       },
       {
-        "x": "car",
-        "y": 109
+        x: 'car',
+        y: 109,
       },
       {
-        "x": "moto",
-        "y": 134
+        x: 'moto',
+        y: 134,
       },
       {
-        "x": "bicycle",
-        "y": 149
+        x: 'bicycle',
+        y: 149,
       },
       {
-        "x": "horse",
-        "y": 111
+        x: 'horse',
+        y: 111,
       },
       {
-        "x": "skateboard",
-        "y": 285
+        x: 'skateboard',
+        y: 285,
       },
       {
-        "x": "others",
-        "y": 195
-      }
-    ]
+        x: 'others',
+        y: 195,
+      },
+    ],
   },
   {
-    "id": "us",
-    "color": "hsl(212, 70%, 50%)",
-    "data": [
+    id: 'us',
+    color: 'hsl(212, 70%, 50%)',
+    data: [
       {
-        "x": "plane",
-        "y": 25
+        x: 'plane',
+        y: 25,
       },
       {
-        "x": "helicopter",
-        "y": 195
+        x: 'helicopter',
+        y: 195,
       },
       {
-        "x": "boat",
-        "y": 174
+        x: 'boat',
+        y: 174,
       },
       {
-        "x": "train",
-        "y": 144
+        x: 'train',
+        y: 144,
       },
       {
-        "x": "subway",
-        "y": 32
+        x: 'subway',
+        y: 32,
       },
       {
-        "x": "bus",
-        "y": 254
+        x: 'bus',
+        y: 254,
       },
       {
-        "x": "car",
-        "y": 237
+        x: 'car',
+        y: 237,
       },
       {
-        "x": "moto",
-        "y": 107
+        x: 'moto',
+        y: 107,
       },
       {
-        "x": "bicycle",
-        "y": 6
+        x: 'bicycle',
+        y: 6,
       },
       {
-        "x": "horse",
-        "y": 133
+        x: 'horse',
+        y: 133,
       },
       {
-        "x": "skateboard",
-        "y": 48
+        x: 'skateboard',
+        y: 48,
       },
       {
-        "x": "others",
-        "y": 245
-      }
-    ]
+        x: 'others',
+        y: 245,
+      },
+    ],
   },
   {
-    "id": "germany",
-    "color": "hsl(293, 70%, 50%)",
-    "data": [
+    id: 'germany',
+    color: 'hsl(293, 70%, 50%)',
+    data: [
       {
-        "x": "plane",
-        "y": 15
+        x: 'plane',
+        y: 15,
       },
       {
-        "x": "helicopter",
-        "y": 45
+        x: 'helicopter',
+        y: 45,
       },
       {
-        "x": "boat",
-        "y": 119
+        x: 'boat',
+        y: 119,
       },
       {
-        "x": "train",
-        "y": 159
+        x: 'train',
+        y: 159,
       },
       {
-        "x": "subway",
-        "y": 165
+        x: 'subway',
+        y: 165,
       },
       {
-        "x": "bus",
-        "y": 224
+        x: 'bus',
+        y: 224,
       },
       {
-        "x": "car",
-        "y": 263
+        x: 'car',
+        y: 263,
       },
       {
-        "x": "moto",
-        "y": 7
+        x: 'moto',
+        y: 7,
       },
       {
-        "x": "bicycle",
-        "y": 76
+        x: 'bicycle',
+        y: 76,
       },
       {
-        "x": "horse",
-        "y": 225
+        x: 'horse',
+        y: 225,
       },
       {
-        "x": "skateboard",
-        "y": 103
+        x: 'skateboard',
+        y: 103,
       },
       {
-        "x": "others",
-        "y": 75
-      }
-    ]
+        x: 'others',
+        y: 75,
+      },
+    ],
   },
   {
-    "id": "norway",
-    "color": "hsl(282, 70%, 50%)",
-    "data": [
+    id: 'norway',
+    color: 'hsl(282, 70%, 50%)',
+    data: [
       {
-        "x": "plane",
-        "y": 19
+        x: 'plane',
+        y: 19,
       },
       {
-        "x": "helicopter",
-        "y": 200
+        x: 'helicopter',
+        y: 200,
       },
       {
-        "x": "boat",
-        "y": 171
+        x: 'boat',
+        y: 171,
       },
       {
-        "x": "train",
-        "y": 94
+        x: 'train',
+        y: 94,
       },
       {
-        "x": "subway",
-        "y": 84
+        x: 'subway',
+        y: 84,
       },
       {
-        "x": "bus",
-        "y": 86
+        x: 'bus',
+        y: 86,
       },
       {
-        "x": "car",
-        "y": 197
+        x: 'car',
+        y: 197,
       },
       {
-        "x": "moto",
-        "y": 254
+        x: 'moto',
+        y: 254,
       },
       {
-        "x": "bicycle",
-        "y": 34
+        x: 'bicycle',
+        y: 34,
       },
       {
-        "x": "horse",
-        "y": 116
+        x: 'horse',
+        y: 116,
       },
       {
-        "x": "skateboard",
-        "y": 69
+        x: 'skateboard',
+        y: 69,
       },
       {
-        "x": "others",
-        "y": 39
-      }
-    ]
-  }
-]
+        x: 'others',
+        y: 39,
+      },
+    ],
+  },
+];
