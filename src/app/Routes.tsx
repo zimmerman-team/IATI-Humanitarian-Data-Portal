b9a11b3cab3d1f8041cc5a380b4aa69fa6b372bf
Stefanos <stefanos@zimmermanzimmerman.nl>
2019-08-26 18:59:51 +0300

    chore: adjust way of including the inner routes container

--- src/app/Routes.tsx
+++ src/app/Routes.tsx
@@ -24,6 +24,10 @@ function Routes() {
         <Route exact path="/about" render={() => <About />} />
         <Route exact path="/faq" render={() => <Faqs />} />
         <Route exact path="/signatory-data" render={() => <SignatoryData />} />
+        <Route
+          path="/signatory-data/:code"
+          render={props => <SubmoduleContainer {...props} />}
+        />
         {/*TODO: im missing!*/}
         <Route
           exact
@@ -39,7 +43,7 @@ function Routes() {
         />
 
         {/* Signatory Data Container*/}
-        <SubmoduleContainer />
+        {/* <SubmoduleContainer /> */}
       </Switch>
     </Suspense>
   );
