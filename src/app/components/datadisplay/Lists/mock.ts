3a8f0b5ef90485c1da049923e369179698d71ba8
fus <martynas@zimmermanzimmerman.nl>
2019-08-28 12:02:59 +0300

    chore: commiting ide stylings

--- src/app/components/datadisplay/Lists/mock.ts
+++ src/app/components/datadisplay/Lists/mock.ts
@@ -1,38 +1,37 @@
 import { ListModel } from './model';
 
-export const listMockData:ListModel = {
-  title:'Activity Summary',
-  subtitle:'Activity Summary subtitle',
+export const listMockData: ListModel = {
+  title: 'Activity Summary',
+  subtitle: 'Activity Summary subtitle',
   items: [
     {
-      label: "For humanitarian activities",
-      tooltip: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      label: 'For humanitarian activities',
+      tooltip:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
       values: [
         {
           qtc: 1,
-          ptc: 2
-        }
+          ptc: 2,
+        },
       ],
     },
     {
-      label: "Has funding recipient details",
+      label: 'Has funding recipient details',
       values: [
         {
           qtc: 3,
-          ptc: 4
-        }
+          ptc: 4,
+        },
       ],
     },
     {
-      label: "With organisation type provided",
+      label: 'With organisation type provided',
       values: [
         {
           qtc: undefined,
-          ptc: 6
-        }
+          ptc: 6,
+        },
       ],
     },
-  ]
+  ],
 };
-
-
--- src/app/components/datadisplay/Lists/model.ts
+++ src/app/components/datadisplay/Lists/model.ts
@@ -3,13 +3,13 @@ export type ListModel = {
   subtitle?: string;
   valueHeaders?: boolean;
   items: ListItemModel[];
-}
+};
 
 export type ListItemModel = {
   label: string;
   values: ListItemValueModel[];
   tooltip?: string;
-}
+};
 
 export type ListItemValueModel = {
   qtc?: number;
@@ -17,7 +17,7 @@ export type ListItemValueModel = {
   date?: Date;
   version?: string;
   highlight?: string;
-}
+};
 
 //TODO: ListItemValueModel should be implemented different
 //-value: oneOf qtc, ptc, date, version
