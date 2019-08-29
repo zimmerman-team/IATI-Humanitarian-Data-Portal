45ab014d3943b1d7012d0a9a919977c84e58d30b
Stefanos <stefanos@zimmermanzimmerman.nl>
2019-08-29 13:52:35 +0300

    chore: adjust sig data outgoing data model

--- src/app/modules/signatory-data/submodules/outgoing/mock.ts
+++ src/app/modules/signatory-data/submodules/outgoing/mock.ts
@@ -4,11 +4,6 @@ import { mockData as horizontalBarChartCardMockData } from 'app/components/surfa
 import { OutgoingModel } from './model';
 
 export const mockData: OutgoingModel = {
-  activity: {
-    name: 'ActionAid UK',
-    code: 'GB-CHC-274467',
-    date: '2017 - 2019',
-  },
   horizontalBarChartCardData: horizontalBarChartCardMockData,
   inPageNavigation: inPageNavMockData,
   lists: [
