efd19f5c8597068e12492acafb9f5a663c02e490
Stefanos <stefanos@zimmermanzimmerman.nl>
2019-08-30 11:56:39 +0300

    feat: data connect providers bar chart

--- src/app/modules/signatory-data/submodules/providersPage/index.tsx
+++ src/app/modules/signatory-data/submodules/providersPage/index.tsx
@@ -1,13 +1,54 @@
+/* core */
 import React from 'react';
+import { withRouter } from 'react-router-dom';
+/* components */
 import { ProvidersPageLayout } from 'app/modules/signatory-data/submodules/providersPage/layout';
+/* state & utils */
+import get from 'lodash/get';
+import { useStoreActions, useStoreState } from 'app/state/store/hooks';
 import { mockData } from './mock';
+import { providersTypesCallValues } from 'app/modules/signatory-data/submodules/providersPage/consts';
+import { getBarChartData } from 'app/modules/signatory-data/submodules/providersPage/utils/getBarChartData';
 
-export function ProvidersPage() {
+export function ProvidersPageFunc(props) {
+  /* redux store variables */
+  const orgtypecodelistData = useStoreState(
+    state => state.orgtypecodelist.data
+  );
+  const sigdataproviderstypesData = useStoreState(
+    state => state.sigdataproviderstypes.data
+  );
+  /* create the API call instances */
+  const orgtypecodelistCall = useStoreActions(
+    state => state.orgtypecodelist.fetch
+  );
+  const sigdataproviderstypesCall = useStoreActions(
+    state => state.sigdataproviderstypes.fetch
+  );
+  /* componentDidMount call */
+  React.useEffect(() => {
+    orgtypecodelistCall({});
+  }, []);
+  React.useEffect(() => {
+    const callValues = {
+      values: {
+        q: `reporting_org_ref:${props.match.params.code}`,
+        'json.facet': JSON.stringify(providersTypesCallValues),
+        rows: 0,
+      },
+    };
+    sigdataproviderstypesCall(callValues);
+  }, [orgtypecodelistData]);
   return (
     <ProvidersPageLayout
       activity={mockData.activity}
-      barChartData={mockData.barChartData}
+      barChartData={getBarChartData(
+        get(sigdataproviderstypesData, 'data', {}),
+        get(orgtypecodelistData, 'data', {})
+      )}
       tableData={mockData.tableData}
     />
   );
 }
+
+export const ProvidersPage = withRouter(ProvidersPageFunc);
