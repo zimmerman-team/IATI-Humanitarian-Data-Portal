f6fd69c5ec370f84dca5cb20a522bf282083a6ae
fus <martynas@zimmermanzimmerman.nl>
2019-09-12 12:15:43 +0300

    feat: signatories publish to iati, progress addition

--- src/app/modules/signatory-progress/utils/formatLineChart.ts
+++ src/app/modules/signatory-progress/utils/formatLineChart.ts
@@ -6,7 +6,40 @@ import { DataPoint, SigItemModel, specPubsItemModel } from './intefaces';
 import { SingleDefGBSignatory } from 'app/state/api/interfaces/gbsignatoryInterface';
 
 /* utils */
-import { checkIfValid, getRealSigCount } from './general';
+import { checkIfValid, getAllSigCount, getRealSigCount } from './general';
+
+export interface FirstDataItemModel {
+  data: DataPoint[];
+  totalSigCount: number;
+}
+
+function formatFirstLine(
+  publisherData: SigItemModel,
+  gbsignatories: SingleDefGBSignatory[]
+): FirstDataItemModel {
+  // so first we get the actual amount of GB
+  // signatories
+  const allSigCount = getAllSigCount(gbsignatories);
+  const firstData: DataPoint[] = [];
+  dateRanges.forEach(range => {
+    const rangeKey = `orgs_[${range.value}]`;
+    // a simple proportion calculation is applied here
+    // to get the percentage value
+    const percentage = Math.round(
+      (publisherData[rangeKey].sigCount * 100) / allSigCount
+    );
+
+    firstData.push({
+      x: range.label,
+      y: percentage,
+    });
+  });
+
+  return {
+    data: firstData,
+    totalSigCount: allSigCount,
+  };
+}
 
 export function formatLineChart(
   publisherData: SigItemModel,
@@ -18,10 +51,19 @@ export function formatLineChart(
     values: { values: [] },
   };
 
-  // so this part forms the line 'Publishing hum. activity data'
   if (publisherData) {
+    // first here we form Signatories publishing
+    // to IATI line by comparing
+    const firstDataItem = formatFirstLine(publisherData, gbsignatories);
+    lineData.values.values.push({
+      data: firstDataItem.data,
+      id: 'Signatories publish to IATI',
+    });
+
+    const allSigCount = firstDataItem.totalSigCount;
+
     specPubsData.forEach(item => {
-      const data: Array<DataPoint> = [];
+      const data: DataPoint[] = [];
 
       dateRanges.forEach(range => {
         const rangeKey = `orgs_[${range.value}]`;
@@ -36,9 +78,19 @@ export function formatLineChart(
 
           // a simple proportion calculation is applied here
           // to get the percentage value
-          percentage = Math.round(
-            (specSigCount * 100) / publisherData[rangeKey].sigCount
-          );
+          // This way the specific sig data would be compared to the
+          // Signatories publishing to IATI amount
+          // which would make the linechart make little sense
+          // cause we would have the signatories publishing humanitarian
+          // data have a bigger percentage than the signatories publishing
+          // data to IATI
+          // percentage = Math.round(
+          //   (specSigCount * 100) / publisherData[rangeKey].sigCount
+          // );
+          // so we'll use this percantage proportion instead
+          // we'll compare the flat amount of signatories to all specific data
+          // as that will be more accurate at least line chart wise
+          percentage = Math.round((specSigCount * 100) / allSigCount);
         }
 
         data.push({
