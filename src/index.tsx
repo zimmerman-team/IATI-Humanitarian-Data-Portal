2687b8bb067eeafb1db430d412ad2a04662f2b0b
Jim Fung <jim@zimmermanzimmerman.nl>
2019-09-13 18:06:06 +0200

    chore: updated config and libraries

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { hydrate, render } from 'react-dom';
 import App from 'app';
-import { setupNotification } from './notification';
+import { setupNotification } from 'notification';
 import * as serviceWorker from 'serviceWorker';
 
 const rootElement = document.getElementById('root');
 
-if (rootElement != null) {
+if (rootElement !== null) {
   if (rootElement.hasChildNodes()) {
     hydrate(<App />, rootElement);
   } else {
@@ -14,10 +14,12 @@ if (rootElement != null) {
   }
 }
 
-// if (process.env.REACT_APP_NODE_ENV === 'development') {
-//   serviceWorker.unregister();
-// } else {
-//   serviceWorker.register();
-// }
+if (process.env.REACT_APP_NODE_ENV === 'development') {
+  serviceWorker.unregister();
+} else {
+  /* for now just do not register the service worker*/
+  serviceWorker.unregister();
+  // serviceWorker.register();
+}
 
 setupNotification();
