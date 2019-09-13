51713cf1971e16b52fb5e025c133025d594dee30
Jim Fung <jim@zimmermanzimmerman.nl>
2019-09-13 18:05:20 +0200

    feat: signatory progrs page background

---
+++ src/app/modules/signatory-progress/common/decoration/DecoSigProgMidLeft.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const DecoSigProgMidLeft = props => (
+  <svg width="32px" height="67px" viewBox="0 0 32 67" {...props}>
+    <g
+      transform="translate(0 -208)"
+      stroke="none"
+      strokeWidth={1}
+      fill="none"
+      fillRule="evenodd"
+    >
+
+      <path transform="translate(0 208)" fill="#ED6060" d="M0 0H32V67H0z" />
+    </g>
+  </svg>
+);
