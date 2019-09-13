0a92af679a86c20592a981ce50b6f3f965e4a7dd
Jim Fung <jim@zimmermanzimmerman.nl>
2019-09-13 18:03:29 +0200

    feat: coverage page background

--- src/app/modules/signatory-data/submodules/coveragePage/layout.tsx
+++ src/app/modules/signatory-data/submodules/coveragePage/layout.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import { CoverageModel } from './model';
 import TableModule from 'app/components/datadisplay/Table';
+import { DecoSigOverviewTopLeft } from 'app/modules/signatory-data/submodules/overview/common/decoration/DecoSigOverviewTopLeft';
+import Box from '@material-ui/core/Box';
 
 export const CoverageLayout = (props: CoverageModel) => {
   return (
     <>
+      {/* ---------------------------------------- */}
+      {/* decoration: top left */}
+      <Box position="absolute" top="0" left="0">
+        <DecoSigOverviewTopLeft />
+      </Box>
+      {/* ---------- */}
+
+      {/* ---------------------------------------- */}
+      {/* Coverage data */}
       <TableModule
         title={props.tableData.title}
         columns={props.tableData.columns}
@@ -12,6 +23,7 @@ export const CoverageLayout = (props: CoverageModel) => {
         data={props.tableData.data}
         options={props.tableData.options}
       />
+      {/* ---------- */}
     </>
   );
 };
