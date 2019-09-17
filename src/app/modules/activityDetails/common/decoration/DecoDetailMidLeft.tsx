12984df44a68bb9a57d41c905531c21e5c4304aa
Jim Fung <jim@zimmermanzimmerman.nl>
2019-09-17 11:19:33 +0200

    feat: activity detail background decorations

---
+++ src/app/modules/activityDetails/common/decoration/DecoDetailMidLeft.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const DecoDetailMidLeft = props => (
+  <svg width="16px" height="583px" viewBox="0 0 16 583" {...props}>
+    <path
+      transform="translate(0 -307)"
+      fill="#6E5ACC"
+      stroke="none"
+      strokeWidth={1}
+      fillRule="evenodd"
+      d="M0 307H16V890H0z"
+    />
+  </svg>
+);
