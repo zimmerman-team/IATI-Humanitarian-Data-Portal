48a6e6289d9ea71ec8367b99dcbdf3eab4431aac
Stefanos <stefanos@zimmermanzimmerman.nl>
2019-09-18 16:01:37 +0300

    feat: align sig progress table 3rd row with design

--- src/app/components/datadisplay/Table/layout.tsx
+++ src/app/components/datadisplay/Table/layout.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 import MUIDataTable from 'mui-datatables';
 
 import { TableLayoutModel } from 'app/components/datadisplay/Table/model';
+import { changeTableRowColor } from 'app/components/datadisplay/Table/helpers';
 
 const TableLayout = (props: TableLayoutModel) => {
+  React.useEffect(() => {
+    props.changeTableRowColor && changeTableRowColor(props.changeTableRowColor);
+  }, [props.changeTableRowColor]);
   return (
     <MUIDataTable
       data={props.data}
