1259c2c654ca1b5ed20ddb38e55b7b738c962a5a
Jim Fung <jim@zimmermanzimmerman.nl>
2019-10-09 14:23:29 +0200

    chore: some minor changes

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -4,37 +4,29 @@ import theme from 'app/theme';
 import { PersistGate } from 'redux-persist/integration/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { StoreProvider } from 'easy-peasy';
-import { appStore, persistor } from './state/store';
+import { appStore, persistor } from 'app/state/store';
 /* todo: refactor, rather not load in "oldscool" css */
 import '../index.css';
-import { Client } from './state/api/Client';
+import { Client } from 'app/state/api/Client';
 import { ClientContextProvider } from 'react-fetching-library';
-import { createGenerateClassName } from '@material-ui/styles';
 
 interface ProviderProps {
   children?: ReactNode;
 }
 
-const generateClassName = createGenerateClassName({
-  disableGlobal: false,
-  productionPrefix: 'production',
-});
-
 function Providers(props: ProviderProps) {
   return (
-
-      <ThemeProvider theme={theme}>
-        {/* redux store provider*/}
-        <StoreProvider store={appStore}>
-          <PersistGate loading={null} persistor={persistor}>
-            <ClientContextProvider client={Client}>
-              {/* react router */}
-              <Router>{props.children}</Router>
-            </ClientContextProvider>
-          </PersistGate>
-        </StoreProvider>
-      </ThemeProvider>
-
+    <ThemeProvider theme={theme}>
+      {/* redux store provider*/}
+      <StoreProvider store={appStore}>
+        <PersistGate loading={null} persistor={persistor}>
+          <ClientContextProvider client={Client}>
+            {/* react router */}
+            <Router>{props.children}</Router>
+          </ClientContextProvider>
+        </PersistGate>
+      </StoreProvider>
+    </ThemeProvider>
   );
 }
 
