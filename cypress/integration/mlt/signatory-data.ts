e17acd7b00a5ffcd37b9d572a88afa4c483f94ff
Stefanos <stefanos@zimmermanzimmerman.nl>
2019-10-21 11:19:51 +0300

    fix: fix sig data test

--- cypress/integration/mlt/signatory-data.ts
+++ cypress/integration/mlt/signatory-data.ts
@@ -3,8 +3,11 @@
 import autoRecord from 'cypress-autorecord';
 
 const signatoryDataTexts = [
-  'Grand Bargain signatories',
-  'An overview of the IATI ("open") data currently published by individual Grand Bargain signatorys or their affiliated organisations. Its primary purpose is to enable Signatories to monitor their own progress in relation to meeting the data publication commitment of the Grand Bargain. Each column in the table relates to a specific IATI publishing indicator as defined within the workstream Core Commitment Target Results & Indicators (CCTRIs).',
+  'Signatory IATI data',
+  "This page provides an overview of the IATI ('open') data currently published by individual Grand Bargain signatories and/or their affiliated organisations. Its primary purpose is to enable signatories to monitor their own progress in relation to meeting the data publication commitment of the Grand Bargain.",
+  // todo: need to find a way to check text with links
+  // 'Each column in the table relates to a specific IATI publishing indicator as defined within the workstream core commitment target results and indicators (CCTRIs). However, it is important to assess the indicators values in the context of how each organisation operates e.g. not all affiliate or other  organisations undertake humanitarian work so may not as yet be publishing any humanitarian information.',
+  // 'For the aggregated view, showing the progress of ALL signatories against the publishing commitment since 1 June 2017, is available here',
   '* NA stands for Not Applicable',
 ];
 
