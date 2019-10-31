bd3f49e6af835666986339ed389e5d6a5a1baa5f
Stefanos <stefanos@zimmermanzimmerman.nl>
2019-10-31 15:59:55 +0200

    feat: render org info on one line

--- src/app/modules/activityDetails/common/activityHeader/index.tsx
+++ src/app/modules/activityDetails/common/activityHeader/index.tsx
@@ -45,9 +45,7 @@ export const ActivityHeaderLayout = (props: ActivityDetailsHeaderCardModel) => {
             color="textPrimary"
             data-testid="organisation"
           >
-            {props.organisation.name} {props.organisation.code}
-            <br />
-            {props.organisation.type}
+            {props.organisation.name} | {props.organisation.code} | {props.organisation.type}
           </Typography>
           <Box height="24px" width="100%" />
         </Grid>
