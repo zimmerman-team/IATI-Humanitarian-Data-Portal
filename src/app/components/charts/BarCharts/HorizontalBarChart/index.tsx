49b8d5123a9abb3da357a3554da9523e5d2a14c1
Juan Albergen <juan@zimmermanzimmerman.nl>
2019-11-08 10:34:18 +0100

    feat: added UI state 'No data found' to HorizontalBarChart
    [MLT-680]
    [MLT-681]

--- src/app/components/charts/BarCharts/HorizontalBarChart/index.tsx
+++ src/app/components/charts/BarCharts/HorizontalBarChart/index.tsx
@@ -4,6 +4,7 @@ import { ResponsiveBar } from '@nivo/bar';
 import styled from 'styled-components';
 import Colours from 'app/theme/color';
 import { colorScheme } from 'app/components/charts/BarCharts/common/colorUtil';
+import Typography from '@material-ui/core/Typography';
 import {
   HorizontalBarChartModel,
   barModel,
@@ -54,26 +55,46 @@ const BarComponent = props => {
   );
 };
 
-const BarChart = styled(props => <ResponsiveBar {...props} />)`
-  && {
-  }
-`;
+const BarChart = styled(props => <ResponsiveBar {...props} />)``;
 
 const ChartContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   height: 400px;
+`;
+
+const NoDataMessage = styled(props => <Typography variant="h6" {...props} />)`
+  && {
+    font-size: 1rem;
+    font-family: Inter;
+    font-weight: 500;
+    line-height: 1.5;
+    letter-spacing: 0.2px;
+  }
 `;
 
 // https://nivo.rocks/bar/
 export const HorizontalBarChart = (props: HorizontalBarChartModel) => {
+
+  function renderBarchart() {
+    if (typeof props.values !== 'undefined' && props.values.length > 0) {
+      return (
+        <BarChart
+          {...barModel}
+          colorBy="index"
+          data={props.values}
+          colors={colorScheme(props.colors)}
+          barComponent={BarComponent}
+        />)
+    }
+
+    return (<NoDataMessage>No data found</NoDataMessage>)
+  }
+
   return (
     <ChartContainer>
-      <BarChart
-        {...barModel}
-        colorBy="index"
-        data={props.values}
-        colors={colorScheme(props.colors)}
-        barComponent={BarComponent}
-      />
+      {renderBarchart()}
     </ChartContainer>
   );
 };
