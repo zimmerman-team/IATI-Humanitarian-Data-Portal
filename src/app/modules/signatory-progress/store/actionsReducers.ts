dde369df09f53c76ac0ba4f09bf9e9358af3f962
fus <martynas@zimmermanzimmerman.nl>
2019-11-11 18:38:33 +0200

    chore: adjusted frontend endpoints

--- src/app/modules/signatory-progress/store/actionsReducers.ts
+++ src/app/modules/signatory-progress/store/actionsReducers.ts
@@ -4,27 +4,19 @@ import { endpoints } from 'app/__consts__/endpoints';
 import { apiModel, spaceCloudAPIModel } from 'app/state/api/actionsReducers';
 
 export const humPublishers: PublisherInterface = {
-  ...apiModel(
-    `${process.env.REACT_APP_DS_API}/search/${endpoints.activity}/select/`
-  ),
+  ...apiModel(`${process.env.REACT_APP_DS_API}/search/${endpoints.activity}/`),
 };
 
 export const publishers202: PublisherInterface = {
-  ...apiModel(
-    `${process.env.REACT_APP_DS_API}/search/${endpoints.activity}/select/`
-  ),
+  ...apiModel(`${process.env.REACT_APP_DS_API}/search/${endpoints.activity}/`),
 };
 
 export const publishers203: PublisherInterface = {
-  ...apiModel(
-    `${process.env.REACT_APP_DS_API}/search/${endpoints.activity}/select/`
-  ),
+  ...apiModel(`${process.env.REACT_APP_DS_API}/search/${endpoints.activity}/`),
 };
 
 export const publishersTrac: PublisherInterface = {
-  ...apiModel(
-    `${process.env.REACT_APP_DS_API}/search/${endpoints.activity}/select/`
-  ),
+  ...apiModel(`${process.env.REACT_APP_DS_API}/search/${endpoints.activity}/`),
 };
 
 export const cctriCMS: GBSignatoryResponseInterface = {
