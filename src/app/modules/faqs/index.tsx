6c3f091258b4dbe6a0a22cdaa13919a71c275307
Stefanos <stefanos@zimmermanzimmerman.nl>
2019-11-19 15:07:19 +0100

    feat: sort faq items by title number

--- src/app/modules/faqs/index.tsx
+++ src/app/modules/faqs/index.tsx
@@ -1,13 +1,21 @@
-import React, { useEffect } from 'react';
+import React from 'react';
+import sortBy from 'lodash/sortBy';
 import { FaqsLayout } from 'app/modules/faqs/layout';
 import { faqStore } from 'app/modules/faqs/store';
 
 export function Faqs() {
   const [state, actions] = faqStore();
 
-  useEffect(() => {
+  React.useEffect(() => {
     actions.getAllItems();
   }, []);
 
-  return <FaqsLayout faqItems={state.faqItems} title="FAQ" />;
+  return (
+    <FaqsLayout
+      faqItems={sortBy(state.faqItems, item =>
+        parseInt(item.title.substring(0, item.title.indexOf('.')), 10)
+      )}
+      title="FAQ"
+    />
+  );
 }
