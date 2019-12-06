5068c84b78680934a7758e5c4bf484aa562dbf08
Stefanos <stefanos@zimmermanzimmerman.nl>
2019-12-06 17:13:08 +0200

    fix: query fix

--- src/app/modules/signatory-data/mock.ts
+++ src/app/modules/signatory-data/mock.ts
@@ -52,7 +52,7 @@ export const iatigbsignatoriesCallValues = {
               OR (-(-sector_vocabulary:1 OR sector_vocabulary:*)
               AND (sector_code:[70000 TO 79999] OR sector_code:[93010 TO 93018]))
               OR (-(-transaction_sector_vocabulary:1 OR transaction_sector_vocabulary:*)
-              AND (transaction_sector_code:[70000 TO 79999] OR transaction_sector_code:[93010 TO 93018]))) AND transaction_provider_org_provider_activity_id:*',
+              AND (transaction_sector_code:[70000 TO 79999] OR transaction_sector_code:[93010 TO 93018]))) AND transaction_provider_org_provider_activity_id:* AND transaction_type:1',
           },
         },
       },
