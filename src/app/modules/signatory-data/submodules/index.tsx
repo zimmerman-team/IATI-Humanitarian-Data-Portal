773388eb7e49a875eec822896a675182382c1dba
Stefanos <stefanos@zimmermanzimmerman.nl>
2019-12-06 15:53:16 +0200

    fix: query fixes

--- src/app/modules/signatory-data/submodules/index.tsx
+++ src/app/modules/signatory-data/submodules/index.tsx
@@ -63,7 +63,7 @@ export function SubmoduleContainer(props) {
       values: {
         q: `reporting_org_ref:${decodeURIComponent(
           props.match.params.code
-        )} AND humanitarian:1 OR transaction_humanitarian:1 OR (-(-sector_vocabulary:1 OR sector_vocabulary:*) AND (sector_code:[70000 TO 79999] OR sector_code:[93010 TO 93018])) OR (-(-transaction_sector_vocabulary:1 OR transaction_sector_vocabulary:*) AND (transaction_sector_code:[70000 TO 79999] OR transaction_sector_code:[93010 TO 93018]))`,
+        )}`,
         facet: 'on',
         'facet.pivot': `${queryDateField},humanitarian`,
         fl: 'facet_counts',
@@ -74,7 +74,7 @@ export function SubmoduleContainer(props) {
       values: {
         q: `reporting_org_ref:${decodeURIComponent(
           props.match.params.code
-        )} AND humanitarian:1 OR transaction_humanitarian:1 OR (-(-sector_vocabulary:1 OR sector_vocabulary:*) AND (sector_code:[70000 TO 79999] OR sector_code:[93010 TO 93018])) OR (-(-transaction_sector_vocabulary:1 OR transaction_sector_vocabulary:*) AND (transaction_sector_code:[70000 TO 79999] OR transaction_sector_code:[93010 TO 93018]))`,
+        )}`,
         'json.facet': JSON.stringify({
           date1: `min(${queryDateField})`,
           date2: `max(${queryDateField})`,
