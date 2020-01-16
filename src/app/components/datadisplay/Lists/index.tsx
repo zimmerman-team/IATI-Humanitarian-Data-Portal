ec7446a62834bd3b42a09ba43391301cbcf31a3f
Juan Albergen <juan@zimmermanzimmerman.nl>
2020-01-16 16:42:41 +0100

    style: text responsiveness list status component

--- src/app/components/datadisplay/Lists/index.tsx
+++ src/app/components/datadisplay/Lists/index.tsx
@@ -8,10 +8,7 @@ import TableRow from '@material-ui/core/TableRow';
 import ListItem from './common/ListItem';
 import { ListModel } from './model';
 import Paper from '@material-ui/core/Paper';
-import { Typography } from '@material-ui/core';
-
-// TODO:
-// - Optional highlight
+import { Typography, useMediaQuery, useTheme } from '@material-ui/core';
 
 export const Base = styled(props => <Paper {...props} />)`
   padding: 8px 28px 32px 28px;
@@ -48,6 +45,8 @@ export const TableSubtitle = styled(props => <Typography {...props} />)``;
 
 export const List = (props: ListModel) => {
   const items = props.items || [];
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.up('md'));
 
   const listItems = items.map(item => (
     <ListItem
@@ -59,6 +58,18 @@ export const List = (props: ListModel) => {
       highlight={item.highlight}
     />
   ));
+
+  function renderValueHeaderText(showHeadersText, header) {
+    if (matches) {
+      return showHeadersText
+        ? (header === 'ptc' && 'Percentage') || (header === 'qtc' && 'Total')
+        : '';
+    } else {
+      return showHeadersText
+        ? (header === 'ptc' && 'Ptc') || (header === 'qtc' && 'Tot')
+        : '';
+    }
+  }
 
   // https://dev.to/claireparkerjones/how-to-create-an-array-of-unique-values-in-javascript-using-sets-5dg6
   // TODO: Refactor code, this logic does not matches the headers with the actual value. Works for now but may cause problems.
@@ -72,9 +83,7 @@ export const List = (props: ListModel) => {
 
     return tableHeadersArray.map(header => (
       <TableValueHeader align="right" key={header as string}>
-        {showHeadersText
-          ? (header === 'ptc' && 'Percentage') || (header === 'qtc' && 'Total')
-          : ''}
+        {renderValueHeaderText(showHeadersText, header)}
       </TableValueHeader>
     ));
   }
