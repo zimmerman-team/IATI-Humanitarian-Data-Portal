4bf8497b6b3ce6f60bf7669823ae97115d4ef0aa
Juan Albergen <juan@zimmermanzimmerman.nl>
2020-01-16 14:53:23 +0100

    chore: filewatcher

--- src/app/state/api/actionsReducers/sigdataoverviewcurrency.ts
+++ src/app/state/api/actionsReducers/sigdataoverviewcurrency.ts
@@ -3,7 +3,9 @@ import { apiModel } from './index';
 import { endpoints } from 'app/__consts__/endpoints';
 
 const sigdataoverviewcurrency: ActivityResponceInterface = {
-  ...apiModel(`${process.env.REACT_APP_DS_API}/search/${endpoints.transaction}/`),
+  ...apiModel(
+    `${process.env.REACT_APP_DS_API}/search/${endpoints.transaction}/`
+  ),
 };
 
 export default sigdataoverviewcurrency;
