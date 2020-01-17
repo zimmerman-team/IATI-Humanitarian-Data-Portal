a59a032960cb8271796d3559781cf993ab807daa
Juan Albergen <juan@zimmermanzimmerman.nl>
2020-01-17 14:03:05 +0100

    feat: responsive footer

--- src/app/components/surfaces/Footer/index.tsx
+++ src/app/components/surfaces/Footer/index.tsx
@@ -1,127 +1,106 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
-import MuiBox from '@material-ui/core/Box';
 import Colors from 'app/theme/color';
-import MuiLink from '@material-ui/core/Link';
-import { DILogo } from 'app/components/svgs/DevelopmentInitiatives';
-import { MONLogo } from 'app/components/svgs/MinistryOfNetherlands';
-import { WBLogo } from 'app/components/svgs/WorldBank';
-import { Grid } from '@material-ui/core';
+import { Grid, Link, useMediaQuery } from '@material-ui/core';
+import { Breakpoints } from 'app/theme';
+import DIImage from 'app/assets/images/Development Initiatives Logo.png';
+import DMImage from 'app/assets/images/Dutch Minsitry of Foreign Affairs Logo.png';
+import WBImage from 'app/assets/images/World Bank Group.png';
+import useTheme from '@material-ui/core/styles/useTheme';
 
-const Box = styled(props => <MuiBox {...props} />)`
+const Wrapper = styled(props => <Grid {...props} />)`
   && {
     background-color: ${Colors.branddark};
     padding: 32px 54px;
-    // position: fixed;
     left: 0;
     bottom: 0;
-    width: calc(100% - 108px);
-  }
-`;
+    margin: 0;
+    justify-content: space-between;
+    align-items: center;
 
-const Link = styled(props => <MuiLink {...props} />)`
-  && {
-    font-family: Inter;
-    font-size: 1rem;
-    font-weight: 500;
-    line-height: 1.5;
-    letter-spacing: 0.5px;
-    color: ${Colors.whiteOrFontlightbase};
-    :hover {
-      cursor: pointer;
+    @media (max-width: ${Breakpoints.sm}px) {
+      justify-content: center;
     }
   }
 `;
 
-const StyledNavLink = styled(props => <NavLink {...props} />)`
-  font-size: 1rem;
+const PrivacyLink = styled(props => <NavLink {...props} />)`
+  text-align: center;
+  font-size: ${props => (props.smDown ? '0.85rem' : '1rem')};
   font-weight: 500;
   line-height: 1.5;
   font-family: Inter;
   letter-spacing: 0.5px;
   color: ${Colors.whiteOrFontlightbase};
 
-  &:hover {
+  &&:hover {
     cursor: pointer;
     text-decoration: underline;
     color: ${Colors.whiteOrFontlightbase};
   }
 `;
 
-export const Footer = () => {
-  return (
-    <Box
-      display="flex"
-      justifyContent="space-between"
-      alignItems="center"
-      data-testid="Footer"
-    >
-      <Link data-testid="DI link" href="http://devinit.org/" target="_blank">
-        <DILogo />
-      </Link>
-
-      <Link
-        data-testid="GOV link"
-        href="https://www.government.nl/ministries/ministry-of-foreign-affairs"
-        target="_blank"
-      >
-        <MONLogo />
-      </Link>
-
-      <Link
-        data-testid="World bank link"
-        href="https://www.worldbank.org/"
-        target="_blank"
-      >
-        <WBLogo />
-      </Link>
-
-      <StyledNavLink data-testid="GB link" to="/privacy">
-        Grand Bargains Data Privacy and Cookie Policy
-      </StyledNavLink>
-    </Box>
-  );
-};
-
-const GridContainer = styled(props => <Grid {...props} />)`
-  && {
-    background-color: ${Colors.branddark};
-    padding: 32px 54px;
-    // position: absolute;
-    left: 0;
-    width: calc(100%);
+const LinkContainer = styled(props => <Grid {...props} />)`
+  @media (max-width: ${Breakpoints.sm}px) {
+    && {
+      margin-top: 62px;
+    }
   }
 `;
 
-export const FooterSM = () => {
-  return (
-    <GridContainer container width="100%">
-      <Grid container item justify="space-between" alignItems="center">
-        <Link data-testid="DI link" href="http://devinit.org/">
-          <DILogo />
-        </Link>
+export const Footer = () => {
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.down('sm'));
 
+  return (
+    <Wrapper container item xs={12} spacing={2}>
+      <Grid item>
+        <Link data-testid="DI link" href="http://devinit.org/" target="_blank">
+          <img
+            src={DIImage}
+            width="100%"
+            height="auto"
+            alt="Development Initiatives Logo"
+          />
+        </Link>
+      </Grid>
+
+      <Grid item>
         <Link
           data-testid="GOV link"
           href="https://www.government.nl/ministries/ministry-of-foreign-affairs"
+          target="_blank"
         >
-          <MONLogo />
+          <img
+            src={DMImage}
+            width="100%"
+            height="auto"
+            alt="Dutch Ministry of Foreign Affairs Logo"
+          />
         </Link>
       </Grid>
 
-      <MuiBox height="24px" width="100%" />
-
-      <Grid container item justify="space-between" alignItems="center">
-        <Link data-testid="World bank link" href="https://www.worldbank.org/">
-          <WBLogo />
+      <Grid item>
+        <Link
+          data-testid="World bank link"
+          href="https://www.worldbank.org/"
+          target="_blank"
+        >
+          <img
+            src={WBImage}
+            width="100%"
+            height="auto"
+            alt="World Bank Group Logo"
+          />
         </Link>
+      </Grid>
 
-        <StyledNavLink data-testid="GB link" to="/privacy">
+      <LinkContainer item>
+        <PrivacyLink data-testid="GB link" to="/privacy" smDown={matches}>
           Grand Bargains Data Privacy and Cookie Policy
-        </StyledNavLink>
-      </Grid>
-    </GridContainer>
+        </PrivacyLink>
+      </LinkContainer>
+    </Wrapper>
   );
 };
