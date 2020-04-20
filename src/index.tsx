ada0a9373a22bdf875570a902e61d2079102fb6c
Jim Fung <jim@zimmermanzimmerman.nl>
2020-04-20 13:46:12 +0200

    chore: updated libs and a small cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -1,28 +1,16 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React from 'react';
-import { hydrate, render } from 'react-dom';
+import ReactDOM from 'react-dom';
 import App from 'app';
 // import { setupNotification } from 'notification';
-// import * as serviceWorker from 'serviceWorker';
+import * as serviceWorker from 'serviceWorker';
 import * as _ from 'styled-components/cssprop';
 
-const rootElement = document.getElementById('root');
+ReactDOM.render(<App />, document.getElementById('root'));
 
-if (rootElement !== null) {
-  if (rootElement.hasChildNodes()) {
-    hydrate(<App />, rootElement);
-  } else {
-    render(<App />, rootElement);
-  }
-}
-
-/*
-if (process.env.REACT_APP_NODE_ENV === 'development') {
-  serviceWorker.unregister();
-} else {
-  /!* for now just do not register the service worker*!/
-  serviceWorker.unregister();
-  // serviceWorker.register();
-}
-*/
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
 
 // setupNotification();
